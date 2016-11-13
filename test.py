a12957d3d7fc7e6ee55330e6c9e5f4d74ae9f5d5
roboticsenthusiast <alanyuyi98@gmail.com>
2016-11-13 00:58:20 -0500

    Extrude tests

--- test.py
+++ test.py
@@ -1,14 +1,6 @@
 from __future__ import division, print_function
 from visual import *
 #bouncing ball
-ball = sphere(pos=(-5,0,0), radius=0.5, color=color.cyan)
-wallR = box(pos=(6,0,0), size=(0.2,12,12), color=color.green)
-ball.velocity=vector(25,0,0)
-deltat=0.005
-t=0
-while t<50:
-    ball.pos=ball.pos+ball.velocity*deltat
-    if ball.pos.x>wallR.pos.x or ball.pos.x<-10:
-        ball.velocity*=-1
-    t+=deltat
-    rate(100)
+plane=shapes.rectangle(width=3,height=5)
+extrusion(shape=plane)
+
