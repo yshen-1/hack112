dd620c7ca87764e460f58c89934e1aeb3f866c3c
roboticsenthusiast <alanyuyi98@gmail.com>
2016-11-12 19:09:24 -0500

    Updated game.py with framework

---
+++ game.py
@@ -0,0 +1,10 @@
+from __future__ import print_function,division
+from visual import *
+
+class game(object):
+    def __init__(self):
+        pass
+    def run(self):
+        pass
+missileCommand=game()
+missileCommand.run()
