d0fe0a39ddfdcd78c64c1f3c3ab20f233c50e6f7
roboticsenthusiast <alanyuyi98@gmail.com>
2016-11-12 19:04:52 -0500

    Made test.py a bouncing ball animation

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
 from __future__ import division, print_function
 from visual import *
-ball=sphere(pos=vector(0,0,0),radius=2,material=materials.earth)
-arrow=arrow(pos=vector(2,0,0),axis=vector(3,0,0),shaftwidth=1)
+#bouncing ball
+ball = sphere(pos=(-5,0,0), radius=0.5, color=color.cyan)
+wallR = box(pos=(6,0,0), size=(0.2,12,12), color=color.green)
+ball.velocity=vector(25,0,0)
+deltat=0.005
+t=0
+while t<3:
+    ball.pos=ball.pos+ball.velocity*deltat
+    if ball.pos.x>wallR.pos.x or ball.pos.x<-10:
+        ball.velocity*=-1
+    t+=deltat
+    rate(100)
