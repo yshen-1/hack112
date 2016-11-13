ea2276e0a97d814db450646b40d4bbd923ba88a7
ben-soudry <soudryb@gmail.com>
2016-11-12 21:17:17 -0500

    Fixed testGenerateMissiles

--- game.py
+++ game.py
@@ -21,8 +21,9 @@ def almostEqual(d1, d2, epsilon=10**-7):
     # note: use math.isclose() outside 15-112 with Python version 3.5 or later
     return (abs(d2 - d1) < epsilon)
 def testGenerateMissile():
-    (x,y,z) = generateMissile
-    assert(almostEqual(x^2+y^2+z^2, 1))
+    (x,y,z) = generateMissile()
+    assert(almostEqual(x**2+y**2+z**2, 1))
+    print("generate missile passed!")
 
 testGenerateMissile()
 
