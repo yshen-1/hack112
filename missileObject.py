4648ac4cb8ade214ddf112359b48f48b35fd38a7
Jerry Yu <jerryyu8@gmail.com>
2016-11-13 09:29:40 -0500

    Fixed missle

--- missileObject.py
+++ missileObject.py
@@ -1,6 +1,22 @@
 from __future__ import print_function,division
 from visual import *
 import math,random
+
+class explosion(object):
+    def __init__(self,location,blastRadius,blastYield):
+        self.over=False
+        self.color=color.yellow
+        self.location=location
+        self.blastRadius=blastRadius
+        self.blastYield=blastYield
+        self.explosion=sphere(pos=self.location,
+                              radius=self.blastRadius+0.0005,color=self.color)
+    def timerFired(self):
+        self.explosion.radius+=0.01
+        if (self.explosion.radius>self.blastYield):
+            self.over=True
+            self.explosion.visible=False
+            del self.explosion
 class missileObject(object):
     def __init__(self,launchLocation,velocity,blastYield,blastRadius=0):
         self.destroyed=False
@@ -11,20 +27,19 @@ class missileObject(object):
         self.velocity=velocity
         self.launchLocation=launchLocation
         self.missileBody=sphere(pos=tuple(launchLocation),
-                                radius=self.radius,color=self.color)
+                                radius=self.radius,color=self.color, make_trail = True)
+        self.missileBody.trail_object.color=color.orange
     def spawnMissiles(self):
         pass
-    def explode(self):
-        self.destroyed=True
-        #(locationX,locationY,locationZ) = self.missileBody.pos
-        #explosionList += (locationX,locationY,locationZ,self.blastRadius)
-
-        explosion=sphere(pos=self.missileBody.pos,
-                         radius=self.blastRadius,color=color.green)
-        del self.missileBody
-        while self.blastRadius<self.blastYield:
-            self.blastRadius+=0.01
-            explosion.radius=self.blastRadius
-        del explosion
-    def timerFired(self,deltaT):
+    def timerFired(self,deltaT,targetRadius):
         self.missileBody.pos+=self.velocity*deltaT
+        if mag(vector(self.missileBody.pos))<targetRadius:
+            self.destroyed=True
+            missileLocation=self.missileBody.pos
+            blastRadius=0
+            blastYield=self.blastYield
+            self.missileBody.visible=False
+            self.missileBody.trail_object.visible=False
+            del self.missileBody.trail_object
+            del self.missileBody
+            return explosion(missileLocation,blastRadius,blastYield)
