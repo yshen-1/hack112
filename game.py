ad7b3acde57ea12a0d9e3605eba3a0618a83bd8b
roboticsenthusiast <alanyuyi98@gmail.com>
2016-11-12 20:12:12 -0500

    Added missileObject class

--- game.py
+++ game.py
@@ -1,11 +1,21 @@
 from __future__ import print_function,division
 from visual import *
-
+class missileObject(object):
+    def __init__(self):
+        pass
+    def explode(self):
+        pass
+    def checkCollision(self):
+        pass
 class game(object):
     def __init__(self):
         self.width=500
-        self.gameScene=display(title="3D missile command",
-                           width=1000,height=500,background=(0,0,0))
+        self.height=500
+        self.sceneCenter=(0,0,0)
+        self.background=(0,0,0)
+        self.gameScene=display(title="3D missile command",width=self.width,
+                               height=self.height,center=self.sceneCenter,
+                               background=self.background)
         self.gameScene.select()
         self.ball = sphere(pos=(0, 0, 0), radius=2, material=materials.earth)
         self.gameOver=False
